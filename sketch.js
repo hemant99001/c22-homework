e46c869354a11c5cdb133aadd08c978601a92cf2
Hemant <79918740+hemant99001@users.noreply.github.com>
2021-04-01 19:45:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(380,230, 10,10,);
+	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -31,21 +31,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
-	 var packageSprite_options ={ 
-        restitution: 1.0 }
-
-	 packageSprite = Bodies.rectangle(380,200,10,10,packageSprite_options);
-	 World.add(world, packageSprite);
-	 packageSprite.scale=0.2
-	 
 
 
 	Engine.run(engine);
@@ -54,23 +46,17 @@ function setup() {
 
 
 function draw() {
-	background(0);
-    Engine.update(engine);
-    rectMode(CENTER);
-    fill("brown");
-	rect(packageSprite.position.x,packageSprite.position.y,25,25);
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
  
 }
 
 function keyPressed() {
- if (keyDown === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	packageSprite.velocityY = -10
-
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
     
   }
 }
-
-
-
